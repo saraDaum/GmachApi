6f1aca33994c6051c6b06f10cea4986f8fe024fa
shanilitov <116593341+shanilitov@users.noreply.github.com>
2023-04-23 11:29:23 +0300

    Add Services Folder

---
+++ Services/ServicesConfing.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class ServicesConfig
+    {
+        public static void AddBLServices(this IServiceCollection collection)
+        {
+            collection.AddScoped(typeof(Repositories.Interfaces.IUserRepository), typeof(Repositories.UserRepository));
+            collection.AddDbContext<Repositories.Models.FullStackMoreshetdbContext>();
+        }
+    }
+}
+
