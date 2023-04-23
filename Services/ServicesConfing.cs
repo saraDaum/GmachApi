0b99b5a9ec4cbfe59df61cc1d72688e7ee641a2b
saraDaum <sara.daum.il@gmail.com>
2023-04-23 11:56:38 +0300

    ---

--- Services/ServicesConfing.cs
+++ Services/ServicesConfing.cs
@@ -11,14 +11,13 @@ namespace Services
     {
         public static void AddBLServices(this IServiceCollection collection)
         {
+            collection.AddScoped(typeof(Repositories.Interfaces.IAcount), typeof(Repositories.Implementation.Account));
+            collection.AddScoped(typeof(Repositories.Interfaces.IBorrower), typeof(Repositories.Implementation.Borrowe));
             collection.AddScoped(typeof(Repositories.Interfaces.ILoanDetail), typeof(Repositories.Implementation.LoanDetail));
             collection.AddScoped(typeof(Repositories.Interfaces.IGuarantor), typeof(Repositories.Implementation.Guarantor));
             collection.AddScoped(typeof(Repositories.Interfaces.IDepositor), typeof(Repositories.Implementation.Depositor));
             collection.AddScoped(typeof(Repositories.Interfaces.IDeposit), typeof(Repositories.Implementation.Deposit));
 
-            collection.AddDbContext<Repositories.Models.GmachimSaraAndShaniContext>();
-            collection.AddScoped(typeof(Repositories.Interfaces.IAcount), typeof(Repositories.Implementation.Account));
-            collection.AddScoped(typeof(Repositories.Interfaces.IBorrower), typeof(Repositories.Implementation.Borrowe));
             collection.AddDbContext<Repositories.Models.GmachimSaraAndShaniContext>();
         }
     }
