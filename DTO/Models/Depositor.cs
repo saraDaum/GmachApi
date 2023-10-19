ee15b93508c3a73640c2aae6f530271185d08197
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-10-19 20:04:04 +0300

    Fix controller route

--- DTO/Models/Depositor.cs
+++ DTO/Models/Depositor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace DTO.Models;
 
-public partial class Depositor:Guarantor
+public partial class Depositor :Guarantor
 {
     /*public int UserNumber { get; set; }
 
