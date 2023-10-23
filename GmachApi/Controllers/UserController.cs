3ac2b30dfa464e0d4220db8fdb5c53ccbdd40f9f
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-10-23 21:01:49 +0300

    Try mix UserController

--- GmachApi/Controllers/UserController.cs
+++ GmachApi/Controllers/UserController.cs
@@ -1,87 +1,99 @@
-﻿using Azure.Identity;
+﻿/*using Azure.Identity;
 using DTO.Models;
-using Microsoft.AspNetCore.Mvc;
 using Repositories.Interfaces;
 using System;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Cors;
+using Repositories.Interfaces;
+using System.Web.Http.Cors;*/
+using Microsoft.AspNetCore.Mvc;
+using DTO.Models;
+/*
 using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
 using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
 using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
-using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;*/
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace GmachApi.Controllers;
-
-//[EnableCors("*", "*", "*")]//Hila added.
-//[Route("api/[controller]")]
-[ApiController]
-public class UserController : ControllerBase
+namespace GmachApi.Controllers
 {
-    Services.IServices.IUser userService;
-    public UserController(Services.IServices.IUser _userService)
+    //[EnableCors("*", "*", "*")] //Hila added.
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : ControllerBase
     {
-        userService = _userService;
+        Services.IServices.IUser userService;
+        // internal Services.IServices.IUser user = new Services.Implemantation.User();
 
+        public UserController(Services.IServices.IUser _userService)
+        {
+            userService = _userService;
+
+        }
+        //Services.IServices.IUser user = new Services.Implemantation.User();
+        // GET: api/<UserController>
+        [HttpGet("GetUserDetail")]
+        public IEnumerable<string> GetUserDetail()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<sign in>
+        /// <summary>
+        ///This function gets a new user and check if a user with these details
+        /// already exist in database before register him.
+        /// </summary>
+        /// <param name="newUser"></param>
+        /// <returns></returns>
+        //[Route("api/User/SignIn"), HttpPost]
+        [HttpPost("SignIn")]
+
+        public int SignIn([FromBody] User newUser)
+
+        {
+            //return 100;
+            Console.WriteLine(newUser.UserName);
+            LoginUser checkUser = new LoginUser { UserName = newUser.UserPassword, Password = newUser.UserPassword };
+
+            if (!userService.IsUserExists(newUser))
+                return userService.SignIn(newUser);
+            else
+                return -1;
+        }
+
+        // POST api/<log in>
+        [HttpPost("LogIn")]
+        public ActionResult<DTO.Models.LoginUser> LogIn([FromBody] LoginUser loginUser)
+        {
+
+            return new LoginUser { UserName = "Connected", Password = "00" }; //To get this message.Sara.
+            return new LoginUser();
+            return NotFound();
+
+
+        }
+
+        // PUT api/<UserController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //    //What this function does?? Sara.
+        //}
+
+        // DELETE api/<UserController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+
+        //}
     }
-    //Services.IServices.IUser user = new Services.Implemantation.User();
-    // GET: api/<UserController>
-    [HttpGet("GetUserDetail")]
-    public IEnumerable<string> GetUserDetail()
-    {
-        return new string[] { "value1", "value2" };
-    }
-
-    // GET api/<UserController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
-
-    // POST api/<sign in>
-    /// <summary>
-    ///This function gets a new user and check if a user with these details
-    /// already exist in database before register him.
-    /// </summary>
-    /// <param name="newUser"></param>
-    /// <returns></returns>
-    [Route("api/User/SignIn"),HttpPost]
-    public int SignIn([FromBody] User newUser)
-    {
-
-        LoginUser checkUser = new LoginUser { UserName = newUser.UserPassword, Password = newUser.UserPassword };
-        if (!userService.IsUserExists(newUser))
-            return userService.SignIn(newUser);
-        else
-            return -1;
-    }
-
-    // POST api/<log in>
-    [HttpPost("LogIn")]
-    public ActionResult<DTO.Models.LoginUser> LogIn([FromBody] LoginUser loginUser)
-    {
-
-        return new LoginUser { UserName = "Connected", Password = "00" }; //To get this message.Sara.
-        return new LoginUser();
-        return NotFound();
-
-
-    }
-
-    // PUT api/<UserController>/5
-    //[HttpPut("{id}")]
-    //public void Put(int id, [FromBody] string value)
-    //{
-    //    //What this function does?? Sara.
-    //}
-
-    // DELETE api/<UserController>/5
-    //[HttpDelete("{id}")]
-    //public void Delete(int id)
-    //{
-
-    //}
 }
--- GmachApi/Program.cs
+++ GmachApi/Program.cs
@@ -75,3 +75,85 @@ app.MapControllers();
 
 app.Run();
 
+
+/*using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Models;
+using Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IDbContext, GmachimSaraAndShaniContext>();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddDbContext<GmachimSaraAndShaniContext>();
+
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(MapperConfig.Instance);
+});
+
+
+builder.Services.AddSingleton(mapperConfig);
+
+var app = builder.Build();
+
+// Add services to the container.
+//ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "docs/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/docs/v1/swagger.json", "My API V1");
+    });
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseRouting();
+
+//app.UseCors("AllowSpecificOrigin"); // השתמש ב-CORS Policy שיצרנו
+
+// global cors policy
+app.UseCors(x => x
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+     .SetIsOriginAllowed(origin => true) // allow any origin 
+     .AllowCredentials());
+
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
+*/
