343b525a3fa7fac30c890252501e38c9c6d239ab
shani litoff <shanilitov@gmail.com>
2023-12-20 18:01:57 +0200

    fix account

--- Repositories/Migrations/20231220155557_changeColumName.Designer.cs
+++ Repositories/Migrations/20231220155557_changeColumName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repositories.Models;
 
@@ -11,9 +12,11 @@ using Repositories.Models;
 namespace Repositories.Migrations
 {
     [DbContext(typeof(GmachimSaraAndShaniContext))]
-    partial class GmachimSaraAndShaniContextModelSnapshot : ModelSnapshot
+    [Migration("20231220155557_changeColumName")]
+    partial class changeColumName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Repositories/Migrations/20231220155557_changeColumName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeColumName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountNunber",
+                table: "Account",
+                newName: "AccountNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountNumber",
+                table: "Account",
+                newName: "AccountNunber");
+        }
+    }
+}
